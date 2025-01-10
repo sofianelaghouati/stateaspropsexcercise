fd364117f3dd721ebbe9baf3c7473b719e264c20
sofianelaghouati <sofianelaghouatipro@gmail.com>
2025-01-10 14:27:15 +0100

    FinalCommit

--- src/Item.js
+++ src/Item.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react'
 
 class Item extends Component{
+    constructor(props){
+        super(props)
+        this.handleRemove = this.handleRemove.bind(this)
+    }
+
+    handleRemove(evt){
+        this.props.remove(this.props.value)
+    }
+
     
     render(){
         return(
-            <li>{this.props.number} <button> X </button> </li>
+            <li>{this.props.value} <button onClick={this.handleRemove}> X </button> </li>
         )
     }
 }
--- src/List.js
+++ src/List.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
 import Item from './Item'
+import './List.css'
+
 
 class List extends Component{
     constructor(props){
@@ -7,12 +9,26 @@ class List extends Component{
         this.state = {
             nums: [1, 2, 3, 4, 5, 6]
         }
+    this.remove = this.remove.bind(this)
+    }
+
+    remove(num){
+        this.setState(st =>({
+            nums: st.nums.filter(n => n!== num)
+        }))
     }
 
     render(){
-        return this.state.nums.map((num)=>
-            <Item number ={num}/>
+        let nums = this.state.nums.map((num)=>
+            <Item value ={num} remove={this.remove}/>
         )
+        return (
+            <div className='List'>
+                <h1>Better number list</h1>
+                <ul>{nums}</ul>
+            </div>
+        )
+
 
     }
 }
